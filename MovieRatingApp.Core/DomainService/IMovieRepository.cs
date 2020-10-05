c14a194c1ea093c451d63bb1cba4d6340575130c
anneluong <54981148+anneluong@users.noreply.github.com>
2020-10-05 08:45:11 +0200

    Developed the service class using TDD
    Contains all the methods required in the assignment.

---
+++ MovieRatingApp.Core/DomainService/IMovieRepository.cs
@@ -0,0 +1,10 @@
+﻿using MovieRatingApp.Core.Entity;
+using System.Collections.Generic;
+
+namespace MovieRatingApp.Core.DomainService
+{
+    public interface IMovieRepository
+    {
+        IEnumerable<Rating> GetAll();
+    }
+}
